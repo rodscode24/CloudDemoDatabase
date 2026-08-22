ec5795ff05224fce5ec6aa586fe56cc4f1171ebc
Rodney Gonzales <312733799+rodscode24@users.noreply.github.com>
2026-08-22 16:19:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-NEXT_PUBLIC_SUPABASE_URL=https://.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=;
+NEXT_PUBLIC_SUPABASE_URL='https://fsqwsosvadlumeysexxt.supabase.co';
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY='sb_publishable_XyOAwx_PjezLhbWmnSFhGg_jA6u9IU-';
